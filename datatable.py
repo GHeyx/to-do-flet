2aa27233235c2b85abae258417245caea99cae03
ernest <ernesty93@gmail.com>
2023-01-18 15:19:45 -0500

    added logic

---
+++ datatable.py
@@ -0,0 +1,41 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(
+        ft.DataTable(
+            columns=[
+                ft.DataColumn(ft.Text("First name")),
+                ft.DataColumn(ft.Text("Last name")),
+                ft.DataColumn(ft.Text("Age"), numeric=True),
+                ft.DataColumn(ft.Text("Gender")),
+            ],
+            rows=[
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text("John")),
+                        ft.DataCell(ft.Text("Smith")),
+                        ft.DataCell(ft.Text("43")),
+                        ft.DataCell(ft.Text("M")),
+                    ],
+                ),
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text("Jack")),
+                        ft.DataCell(ft.Text("Brown")),
+                        ft.DataCell(ft.Text("19")),
+                        ft.DataCell(ft.Text("M")),
+                    ],
+                ),
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text("Alice")),
+                        ft.DataCell(ft.Text("Wong")),
+                        ft.DataCell(ft.Text("25")),
+                        ft.DataCell(ft.Text("F")),
+                    ],
+                ),
+            ],
+        ),
+    )
+
+ft.app(target=main)
---
+++ main.py
@@ -0,0 +1,37 @@
+# 3 sections
+#     Section 1: Family Tree View or Data Table View
+#     Section 2: On Click, show the details of the person in famiily as card view 
+#     Section 3: Relationship View of source and target
+
+
+# Section 1 ----------------TOP SECTION-----------------------------------
+# treeview tab
+# from data generate family tree
+    # familyid = fatherfirstname + motherfirstname + lastname
+    # family color palette
+
+# dataview tab
+#   addmember.py
+#     add, edit, delete
+
+
+
+
+
+# Section 2 -----------------MIDDLE SECTION------------------------------------
+# treeview tab
+#   Card for selected family member
+# dataview tab
+#   Highlight in datatable seclected family member
+
+
+
+
+
+
+
+# Section 3 -----------------BOTTOM SECTION-------------------------------------
+# treeview tab
+#   Select family tree node for source
+#   Select family tree node for target
+#   Relationship view
