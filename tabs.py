48040f7010292ad332ef3dfa22e8bb927caf8982
ernest <ernesty93@gmail.com>
2023-01-19 00:04:48 -0500

    spaghet

--- tabs.py
+++ tabs.py
@@ -115,6 +115,13 @@ class class1(ft.UserControl):
             expand=1,
     )
 
+class person:
+    def __init__(self, first_name, last_name, date_of_birth, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+
 def main(page: ft.Page):
     page.title = "Tabs Example"
     
