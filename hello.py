b26e86dfad4269f7f415aa02b09e1c9a1f5849cd
ernest <ernesty93@gmail.com>
2023-01-18 20:33:10 -0500

    chaos main

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ def main(page: ft.Page):
     page.add(ft.Text(value="Hello, world!"))
 
 ft.app(target=main)
+
+import flet as ft
+
+ft.app(target=main)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import flet as ft
+from flet import *
 # 3 sections
 #     Section 1: Family Tree View or Data Table View
 #     Section 2: On Click, show the details of the person in famiily as card view 
@@ -35,3 +37,91 @@
 #   Select family tree node for source
 #   Select family tree node for target
 #   Relationship view
+
+class person(ft.UserControl):
+    def __init__(self, first_name):
+        super().__init__()
+        self.first_name = first_name
+
+    def build(self):
+        self.display_table = ft.DataTable(
+            columns=[
+                ft.DataColumn(ft.Text("First name")),
+            ],
+            rows=[
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text("John")),
+                    ],
+                ),
+            ],
+        )
+        return ft.DataTable()
+
+class tree(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+        image = ft.Image("https://www.w3schools.com/howto/img_avatar.png")
+
+    def build(self):
+        return super().build() 
+
+
+class ELK(ft.UserControl):
+    def build(self):
+        self.view = ft.Tabs(
+            selected_index=0,
+            animation_duration=185,
+            tabs=[
+                ft.Tab(
+                    text="Tree",
+                    icon=ft.icons.NATURE,
+                    # tooltip="View Family Tree" #throws error
+                    content=ft.Container(
+                        content=ft.Text("This is Tab 1"), 
+                        alignment=ft.alignment.center
+                    ),
+                ),
+                ft.Tab(
+                    text="Data",
+                    icon=ft.icons.TABLE_VIEW,
+                    # tooltip="View and Edit Family Data", #throws error
+                    content=ft.Container(
+                        content=ft.Text("This is Tab 2"),
+                        alignment=ft.alignment.center,
+                        ),
+                ),
+                
+            ],
+            expand=1,
+        )
+
+        return self.view
+    
+    def update(self):
+        # status = self.view.tabs[self.view.selected_index].text
+        super().update()
+
+    def tabs_changed(self, e):
+        self.update()
+    #     page.add(t)
+
+
+def main(page: Page):
+    #Center window on open and set window size
+    page.window_center()
+    page.window_width = 800
+    page.window_height = 800
+
+    page.title = "Family Tree"
+    page.update()
+
+    # create application instance
+    app = ELK()
+
+
+    # add application's root control to the page
+    page.add(app)
+
+
+ft.app(target=main)
