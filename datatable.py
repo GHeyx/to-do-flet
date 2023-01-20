409680f2621ad62cd5d179a3b1e0bdc011ba83f7
ernest <ernesty93@gmail.com>
2023-01-20 17:24:11 -0500

    add first name, see datatable update in real time

--- datatable.py
+++ datatable.py
@@ -1,41 +1,59 @@
 import flet as ft
 
+
+
 def main(page: ft.Page):
-    page.add(
-        ft.DataTable(
-            columns=[
-                ft.DataColumn(ft.Text("First name")),
-                ft.DataColumn(ft.Text("Last name")),
-                ft.DataColumn(ft.Text("Age"), numeric=True),
-                ft.DataColumn(ft.Text("Gender")),
-            ],
-            rows=[
-                ft.DataRow(
-                    cells=[
-                        ft.DataCell(ft.Text("John")),
-                        ft.DataCell(ft.Text("Smith")),
-                        ft.DataCell(ft.Text("43")),
-                        ft.DataCell(ft.Text("M")),
-                    ],
-                ),
-                ft.DataRow(
-                    cells=[
-                        ft.DataCell(ft.Text("Jack")),
-                        ft.DataCell(ft.Text("Brown")),
-                        ft.DataCell(ft.Text("19")),
-                        ft.DataCell(ft.Text("M")),
-                    ],
-                ),
-                ft.DataRow(
-                    cells=[
-                        ft.DataCell(ft.Text("Alice")),
-                        ft.DataCell(ft.Text("Wong")),
-                        ft.DataCell(ft.Text("25")),
-                        ft.DataCell(ft.Text("F")),
-                    ],
-                ),
-            ],
-        ),
+    #Center window on open and set window size
+    page.window_center()
+    page.window_height = 1000
+    page.window_width = 1000
+    page.scroll = "adaptive"
+
+    def add_person(e):
+        family_table.columns.append(ft.DataRow(cells = [ft.DataCell(ft.Text(new_person.value))]))
+        new_person.value = ""
+        try:
+            view.update() 
+            table_view.update()
+        except:
+            pass
+
+    family_table = ft.DataTable(
+        columns=[
+            # Person Attributes: First, Last, DOB, Gender, etc.
+            ft.DataColumn(ft.Text("First name"))
+        ],
+        rows=[
+            ft.DataRow(cells = [ft.DataCell(ft.Text("John"))]),
+        ]
     )
+
+    new_person = ft.TextField(hint_text="First Name", expand=True)
+    new_person_button = ft.ElevatedButton(text="Add", on_click=add_person, icon=ft.icons.ADD)
+
+
+    table_view=ft.Column(
+            controls=[
+                family_table
+            ]
+    )
+    view = ft.Column(
+        spacing=50,
+        tight=True,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_person,
+                    new_person_button
+                ],
+            ),
+            family_table
+        ],
+        
+    )
+
+    page.add(view)
+
+
 
 ft.app(target=main)
