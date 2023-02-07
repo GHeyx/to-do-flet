ef28bdb36981f5cfa223e92f423ba8ffa9e34b23
ernest <ernesty93@gmail.com>
2023-02-07 18:33:29 -0500

    overlay example added

--- graph_test.py
+++ graph_test.py
@@ -50,6 +50,41 @@ def main(page: ft.Page):
                     hoverinfo='text',
                     opacity=0.8
                     ))
+    
+    def make_annotations(pos, text, font_size=10, font_color='rgb(250,250,250)'):
+        L=len(pos)
+        if len(text)!=L:
+            raise ValueError('The lists pos and text must have the same len')
+        annotations = []
+        for k in range(L):
+            annotations.append(
+                dict(
+                    text=labels[k], # or replace labels with a different list for the text within the circle
+                    x=pos[k][0], y=2*M-position[k][1],
+                    xref='x1', yref='y1',
+                    font=dict(color=font_color, size=font_size),
+                    showarrow=False)
+            )
+        return annotations
+    
+    axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            )
+
+    fig.update_layout(title= 'Tree with Reingold-Tilford Layout',
+                annotations=make_annotations(position, v_label),
+                font_size=12,
+                showlegend=False,
+                xaxis=axis,
+                yaxis=axis,
+                margin=dict(l=40, r=40, b=85, t=100),
+                hovermode='closest',
+                plot_bgcolor='rgb(248,248,248)'
+                )
+    fig.show()
+    
     page.add(PlotlyChart(fig, expand=True))
 
 ft.app(target=main)
